fbc90be5bb10c73430203bc5d80ab4d37234c0b5
Otto Schnurr <otto.schnurr@gmail.com>
2015-12-09 20:19:12 -0500

    Add empty command line tool project.

---
+++ MatrixMultiplication/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MatrixMultiplication
+//
+//  Created by Otto Schnurr on 12/9/15.
+//  Copyright © 2015 Otto Schnurr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
