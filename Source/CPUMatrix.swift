b0714820eec6a87b05e4e4b02c1e34325d5fd4cc
Otto Schnurr <otto.schnurr@gmail.com>
2015-12-14 09:54:02 -0500

    Add preliminary CPUMatrix.
    Not yet implemented.

---
+++ Source/CPUMatrix.swift
@@ -0,0 +1,25 @@
+//
+//  CPUMatrix.swift
+//
+//  Created by Otto Schnurr on 12/14/2015.
+//  Copyright © 2015 Otto Schnurr. All rights reserved.
+//
+//  MIT License
+//     file: ../LICENSE.txt
+//     http://opensource.org/licenses/MIT
+//
+
+class CPUMatrix: PaddedMatrix {
+
+    let rowCount = 0
+    let columnCount = 0
+    let bytesPerRow = 0
+    
+    var baseAddress: UnsafePointer<Float32> {
+        // !!!: implement me
+        return nil
+    }
+
+    let byteCount = 0
+
+}
